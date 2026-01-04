0e612d17ee4c4ef739c07102b26c0ebd1e3f00f5
sathwika-sree8 <sathwikasree18@gmail.com>
2026-01-04 19:31:17 +0530

    fix View props type

--- components/View.tsx
+++ components/View.tsx
@@ -4,7 +4,11 @@ import {client} from "@/sanity/lib/client";
 import {STARTUP_VIEWS_QUERY} from "@/sanity/lib/queries";
 import { writeClient } from '@/sanity/lib/write-client';
 
-const View = async({id}:{id:string}) => {
+interface ViewProps {
+  id: string;
+  views: number;
+}
+const View = async ({ id, views }: ViewProps) => {
   const result = await client.withConfig(
     {useCdn:false}).fetch(STARTUP_VIEWS_QUERY,{id});
 
